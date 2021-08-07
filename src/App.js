f7156cac71a6ccc23338fb6b6ff62db518f8597e
saraalshater <gleave.alshater@gmail.com>
2021-08-07 18:54:26 +0300

    lab8 with errors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
+// import Weather from './components/Weather';
 
 class App extends React.Component {
 
@@ -15,10 +16,13 @@ class App extends React.Component {
       showMap: false,
       errorMsg: '404 error',
       displayError: ' ',
-      city:'',
+      city: '',
       weatherStrings: [],
       weatherError: false,
       weatherText: "",
+      weather: [],
+      movies: [],
+      showCard: true
     };
   }
 
@@ -31,8 +35,12 @@ class App extends React.Component {
 
     let cityName = event.target.city.value;
 
+
     let url = `https://eu1.locationiq.com/v1/search.php?key=pk.a18c8cb52ca73ae8d6046e9f06b14aae&q=${cityName}&format=json`;
 
+    let weatherURL = `http://localhost:3002/weather?lat=${this.state.lat}&lon=${this.state.lon}&cityname=${cityName}`
+
+    let movieUrl = `http://localhost:3002/movies?cityname=${cityName}`;
 
     try {
 
@@ -51,11 +59,25 @@ class App extends React.Component {
 
 
       });
+
+      let weatherResult = await axios.get(weatherURL);
+
+      this.setState({
+        weather: weatherResult.data.data,
+        showCard: true
+      })
+
+      let movieResult = await axios.get(movieUrl);
+      this.setState({
+        movies: movieResult.data,
+        
+      })
+
     }
     catch {
 
       this.setState({
-        showMap: false,
+        // showMap: false,
         displayError: true
 
       });
@@ -66,41 +88,42 @@ class App extends React.Component {
 
   }
 
-  // ------------------lab07=====================================
-  handleWeather = async () =>{
+  // ------------------
+  // handleWeather = async () =>{
 
-    // http://localhost:3001/weather?lat= &lon= &searchQuery=amman
-    let URL= `http://localhost:3001/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.cityName}`;
-    
-    let weatherData= await axios.get(URL);
-    
-    let arrayOfStrings = weatherData.data.map((element) => {
-      return `The Date is : ${element.date} and the description is : ${element.description}`;
-    });
-    
-    try{
-      this.setState({
-        weather: weatherData.data,
-        weatherStrings: arrayOfStrings,
-      });
-    }
-    
-    catch (err) {
-      console.log(err);
-          this.setState({
-            weatherError: true,
-            weatherText: "There is an Error",
-          });
-    }
-    
-    };
+  //   // http://localhost:3001/weather?lat= &lon= &searchQuery=amman
+  //   let URL= `http://localhost:3001/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.cityName}`;
 
+  //   let weatherData= await axios.get(URL);
 
+  //   let arrayOfStrings = weatherData.data.map((element) => {
+  //     return `The Date is : ${element.date} and the description is : ${element.description}`;
+  //   });
 
+  //   try{
+  //     this.setState({
+  //       weather: weatherData.data,
+  //       weatherStrings: arrayOfStrings,
+  //     });
+  //   }
 
+  //   catch (err) {
+  //     console.log(err);
+  //         this.setState({
+  //           weatherError: true,
+  //           weatherText: "There is an Error",
+  //         });
+  //   }
+
+  //   };
+
+  // ================
 
 
-// ================================================================
+
+
+
+
   render() {
     return (
       <>
@@ -116,25 +139,51 @@ class App extends React.Component {
         </Form>
 
 
-        {this.state.displayName}
+
 
 
         {this.state.showMap &&
           < img src={`https://maps.locationiq.com/v3/staticmap?key=pk.a18c8cb52ca73ae8d6046e9f06b14aae&center=${this.state.lat},${this.state.lon}`} alt='map' />}
 
 
-<p>
-{this.state.displayName}
-</p>
+        <p>
+          {this.state.displayName}
+        </p>
 
-<p>
-lat : {this.state.lat}
-</p>
+        <p>
+          {this.state.lat}
+        </p>
 
-<p>
-Lon : {this.state.lon}
-</p>
+        <p>
+          {this.state.lon}
+        </p>
 
+        {/* <Weather showCard={this.state.showCard} weather={this.state.weather} ></Weather> */}
+
+
+        {this.state.showCard &&
+          this.state.weather.map(item => {
+            return (
+              `date :${item.date} and description: ${item.description}`
+            )
+          })}
+
+
+
+
+        {this.state.showCard &&
+          this.state.movies.map(item => {
+            return (
+              `title :${item.title},
+     overview:${item.overview} ,
+    average_votes:${item.average_votes},
+    total_votes:${item.total_votes},
+    image_url: ${item.image_url},
+    popularity: ${item.popularity},
+    released_on:${item.released_on}
+    `
+            )
+          })}
       </>
 
 
@@ -148,3 +197,174 @@ Lon : {this.state.lon}
 
 
 export default App;
+
+
+// =============================================================================================================
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+// import axios from 'axios';
+
+// class App extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       displayName: ' ',
+//       lon: ' ',
+//       lat: ' ',
+//       showMap: false,
+//       errorMsg: '404 error',
+//       displayError: false,
+//       showCard: false,
+//       // city:'',
+//       weather: [],
+//       // weatherError: false,
+//       // weatherText: "",
+
+//     };
+//   }
+
+
+
+
+//   getTheCity = async (event) => {
+//     event.preventDefault();
+//     let cityName = event.target.city.value;
+//     let url = `https://eu1.locationiq.com/v1/search.php?key=pk.a18c8cb52ca73ae8d6046e9f06b14aae&q=${cityName}&format=json`;
+
+//     let urlWeather = `http://localhost:3000/weather?lat=${this.state.lat}&lon=${this.state.lon}&cityname=${cityName}`;
+
+//     try {
+
+//       let cityResult = await axios.get(url);
+
+
+//       this.setState({
+//         displayName: cityResult.data[0].display_name,
+//         lon: cityResult.data[0].lon,
+//         lat: cityResult.data[0].lat,
+//         showMap: true,
+//         showCard: true,
+//       });
+
+
+
+//       let weatherResult = await axios.get(urlWeather);
+
+//       this.setState({
+//         weather: weatherResult.data
+//       });
+
+
+
+
+//     }
+//     catch {
+
+//       this.setState({
+//         showMap: false,
+//         displayError: true,
+//         showCard: true
+
+//       });
+
+//     }
+
+
+
+//   }
+
+//   // ------------------lab07=====================================
+//   // handleWeather = async () => {
+
+//     //   // http://localhost:3001/weather?lat= &lon= &searchQuery=amman
+//     //   let URL= `http://localhost:3001/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=${this.state.cityName}`;
+
+//     //   let weatherData= await axios.get(URL);
+
+//     // let arrayOfStrings = weatherData.data.map((element) => {
+//     //   return `The Date is : ${element.date} and the description is : ${element.description}`;
+//     // });
+
+//     //   try{
+//     //     this.setState({
+//     //       weather: weatherData.data,
+//     //       weatherStrings: arrayOfStrings,
+//     //     });
+//     //   }
+
+//     //   catch (err) {
+//     //     console.log(err);
+//     //         this.setState({
+//     //           weatherError: true,
+//     //           weatherText: "error",
+//     //         });
+//     //   }
+
+//   // };
+
+
+
+
+
+
+//   // ================================================================
+//   render() {
+//     return (
+//       <>
+//         <h1>City Explorer</h1>
+//         <Form onSubmit={this.getTheCity}>
+//           <Form.Group className="mb-3" controlId="formBasicEmail">
+//             <Form.Label>Explore a City :)</Form.Label>
+//             <Form.Control type="text" placeholder="Enter a city name" name="city" />
+//           </Form.Group>
+//           <Button variant="primary" type="submit">
+//             Explore!
+//           </Button>
+//         </Form>
+
+
+//         {this.state.displayName}
+
+
+//         {this.state.showMap &&
+//           < img src={`https://maps.locationiq.com/v3/staticmap?key=pk.a18c8cb52ca73ae8d6046e9f06b14aae&center=${this.state.lat},${this.state.lon}`} alt='map' />}
+
+
+//         <p>
+//           {this.state.displayName}
+//         </p>
+
+//         <p >
+//           lat : {this.state.lat}
+//         </p>
+
+//         <p>
+//           Lon : {this.state.lon}
+//         </p>
+
+//         {/* <Weather showCard={this.state.showCard} weather={this.state.weather} ></Weather> */}
+
+
+//         {this.state.showCard &&
+//         this.state.weather.map(item =>{
+//           return (
+//             `date :${item.date} and description: ${item.description}`
+//           )
+//         })}
+
+//       </>
+
+
+
+//     )
+//   }
+
+// }
+
+
+
+
+// export default App;
---
+++ src/components/Movies.js
@@ -0,0 +1,57 @@
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Card from 'react-bootstrap/Card'
+
+
+// class Movies extends React.Component {
+
+
+//     render() {
+//         return (
+//             <>
+// {
+//                    this.props.showCard && 
+//                     this.props.movies.map(element => {
+//                         return (<Card style={{ border: 'groove', width: '500px' }} >
+//                             <Card.Header  as="h5">
+//                                 {element.title}
+//                             </Card.Header>
+//                             <Card.Header  as="h5">
+//                                 <img src = {element.image_url}></img>
+//                             </Card.Header>
+//                             <Card.Header  as="h5">
+//                                Overview : {element.overview}
+//                             </Card.Header>
+                           
+//                             <Card.Header  as="h5">
+//                                Average votes : {element.average_votes}
+//                             </Card.Header>
+//                             <Card.Header  as="h5">
+//                               Total votes :  {element.total_votes}
+//                             </Card.Header>
+                          
+//                             <Card.Header  as="h5">
+//                               Popularity :  {element.popularity}
+//                             </Card.Header>
+
+//                             <Card.Header  as="h5">
+//                               Released on :  {element.released_on}
+//                             </Card.Header>
+//                         </Card>
+//                         )
+//                     })
+//                 }
+//             </>
+
+//         )
+
+//     }
+
+// }
+
+
+
+
+
+
+// export default Movies;
